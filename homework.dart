e497556cd168107383d94c7fd4f9fec8c5570d3b
marvelous188 <117350310+marvelous188@users.noreply.github.com>
2022-12-07 13:32:42 +0100

    Add files via upload

---
+++ homework.dart
@@ -0,0 +1,40 @@
+import'dart:io';
+
+import '../lib/math/math.dart';
+
+
+void main() {
+  
+  float Princial;
+
+  float Rate;
+
+  float Time;
+
+  float Amount
+
+  let Comund Interest;
+
+  stdout.write("Enter the Principal");
+
+  Principal = float.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter the Rate");
+
+  Rate = float.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter the Time");
+
+  Time = float.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter the Amount");
+
+  Amount = float.parse(stdin.readLineSync()!);
+  print (A = P*(pow((1 + R/100), T)));
+
+  Compound Interest = A - P;
+
+  print("Value is: $Compound Interest ");
+
+
+}
